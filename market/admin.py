46f23d6ef71f00ae3a3e88bca6b9e496d078d69f
Hugo Moisés <hugo2018moises@gmail.com>
2025-08-22 14:47:04 -0300

    Adicionando os models de venda e pagamento. Adicionado função de registro de pagamento e venda para atualizar o saldo do cliente.

--- market/admin.py
+++ market/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Client
+from .models import User, Client, Sale, Payment
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Client)
+admin.site.register(Sale)
+admin.site.register(Payment)
--- market/models.py
+++ market/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
 class User(models.Model):
     TYPES_USER = [
@@ -20,10 +20,51 @@ class Client(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     cpf = models.CharField(max_length=11)
-    telefone = models.CharField(max_length=13)
-    endereco = models.CharField(max_length=200)
+    phone = models.CharField(max_length=13)
+    address = models.CharField(max_length=200)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
-        return f"Cliente {self.id}: {self.name}"
-    
+        return f"Cliente {self.id}: {self.name} - Saldo devedor: R${self.balance}. "
+
+class Sale(models.Model):
+    id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE) 
+    total_value = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.client.balance += self.total_value
+            self.client.save()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Venda de R${self.total_value} para {self.client.name}."
+
+
+class Payment(models.Model):
+    id = models.AutoField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    paid_value = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        if self.client.balance <= 0:
+            raise ValidationError("O cliente não tem saldo devedor.")
+        
+        if self.paid_value <= 0:
+            raise ValidationError("O valor pago deve ser maior que 0.")
+        
+        if self.paid_value > self.client.balance:
+            raise ValidationError("O valor pago não pode ser maior que o saldo devedor do cliente.")
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.client.balance -= self.paid_value
+            self.client.save()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Pagamendo de R${self.paid_value} feito por {self.client.name}."
 
